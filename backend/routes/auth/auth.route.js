2ecf178194ba01052ab0d9f706f7e17d19cb45ed
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2024-12-27 17:07:59 +0530

    Auth Backend is Done

---
+++ backend/routes/auth/auth.route.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  authMiddleware,
+} from "../../controllers/auth.controller.js";
+
+const router = express.Router();
+
+// Route for registering a new user
+router.post("/register", registerUser);
+
+// Route for logging in a user
+router.post("/login", loginUser);
+
+// Route for logging out a user
+router.post("/logout", logoutUser);
+
+// Protected route to check authentication status
+router.get("/check-auth", authMiddleware, (req, res) => {
+  try {
+    const user = req.user;
+    res.status(200).json({
+      success: true,
+      message: "Authenticated user!",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error verifying authentication",
+      error: error.message,
+    });
+  }
+});
+
+export default router;
